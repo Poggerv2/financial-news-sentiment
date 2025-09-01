999a21a57693c83d5c9834a895b111c27732fb17
Poggerv2 <poggervtwo@gmail.com>
2025-09-01 19:20:02 -0300

    Parsers y collectors completados

--- scripts/collect_data_social.py
+++ scripts/collect_data_social.py
@@ -1,35 +1,39 @@
 import requests
 import praw
+from newspaper import Article, Config
+import time
+import random
 import tweepy
 from datetime import datetime
+from dotenv import load_dotenv
 import hashlib
 import json
+import os
 from pathlib import Path
-from parser_social import parse_reddit_post, parse_tweet
+from parser_social import parse_reddit, parse_tweet
 from db import get_mongo_collection
+
+load_dotenv()
+#twitter
+bearer_token = os.getenv("bearer_token")
+#reddit
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+user_agent = os.getenv("user_agent")
 
 data_path = Path(__file__).resolve().parents[1] / "data" / "raw"
 data_path.mkdir(parents=True, exist_ok=True)
 
 articles_col = get_mongo_collection()
 
-def collect_reddit(subreddit_name="cryptocurrency", limit=200, allowed_flairs=None):
-    reddit = praw.Reddit(
-        client_id="wcgoWTJ2JQdpvKvWDWq0fg",
-        client_secret="ipr9OEOC6USZ3iALk1DRnLkMIBMXQw",
-        user_agent="financial-news-sentiment"
-    )
-    posts = reddit.subreddit(subreddit_name).new(limit=limit)
-    results = []
-    for post in posts:
-        parsed = parse_reddit_post(post, allowed_flairs=allowed_flairs)
-        if parsed:
-            results.append(parsed)
+def collect_reddit_news(subreddit="CryptoCurrency", limit=100, flair="GENERAL-NEWS"):
+    results = list(parse_reddit(subreddit, limit, flair))
+    print(f"\nCollected {len(results)} reddit articles")
     return results
 
 def collect_twitter(query="bitcoin OR BTC OR crypto", limit=50):
-    client = tweepy.Client(bearer_token="AAAAAAAAAAAAAAAAAAAAAPeO3wEAAAAAS7lfw1Z%2F1aENBLB5mYf6AW5hB14%3DiHWH3AqioR36UqQDppVcdrZkW7473xFe6VAVy4d8CZLRhbOff9")
-    
+    client = tweepy.Client(bearer_token=bearer_token)
+
     tweets = client.search_recent_tweets(
         query=query,
         max_results=min(limit, 50),
@@ -46,7 +50,7 @@ def save_json(data, source="cronista"):
     file_path = data_path / f"{source}_{today}.json"
     with open(file_path, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, default=str, indent=2)
-    print(f"Guardados {len(data)} artículos en {file_path}")
+    print(f"Saved {len(data)} articles in {file_path}")
 
 def save_mongo(data):
     if not data:
@@ -56,15 +60,15 @@ def save_mongo(data):
 
 # --- Main ---
 def main():
-    reddit_data = collect_reddit("cryptocurrency", limit=200, allowed_flairs=["GENERAL-NEWS"])
-    #twitter_data = collect_twitter(limit=50)
-    
-    all_data = reddit_data# + twitter_data
-    
+    reddit_data = collect_reddit_news("CryptoCurrency", limit=100)
+    # twitter_data = collect_twitter(limit=50)  # comentamos hasta que se restablezca la cuota
+
+    all_data = reddit_data  # + twitter_data cuando esté disponible
     save_json(all_data, source="social")
     save_mongo(all_data)
-    print(f'Total articulos reddit: {len(reddit_data)}')
-    #print(f'Total articulos twitter: {len(twitter_data)}')
+
+    print(f'Reddit: {len(reddit_data)}')
+    # print(f'Twitter: {len(twitter_data)}')
 
 if __name__ == "__main__":
     main()
